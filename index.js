270793a293a8a1dd8395f8bcad057935a259f0b3
David Kim <davidjkim810@gmail.com>
2017-08-30 15:58:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = "I don't do much."
 
-function destructivelyAppendKitten(name) {
-  name.push("kitten")
+function destructivelyAppendKitten(kittens) {
+  [kittens].push("kitten")
   return destructivelyAppendKitten(kittens)
 }
